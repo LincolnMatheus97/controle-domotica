f831286728dd4c64615de7665ee38c840942ec55
jpaullopes <jpaullopes27@gmail.com>
2025-08-26 21:30:03 -0300

    Add schemes in the app

---
+++ backend/app/schemes/__init__.py
@@ -0,0 +1,11 @@
+from .comodo import ComodoCreate, ComodoUpdate, ComodoResponse
+from .dispositivo import DispositivoCreate, DispositivoUpdate, DispositivoResponse
+from .cena import CenaCreate, CenaUpdate, CenaResponse
+from .acao import AcaoCreate, AcaoUpdate, AcaoResponse
+
+__all__ = [
+    "ComodoCreate", "ComodoUpdate", "ComodoResponse",
+    "DispositivoCreate", "DispositivoUpdate", "DispositivoResponse",
+    "CenaCreate", "CenaUpdate", "CenaResponse",
+    "AcaoCreate", "AcaoUpdate", "AcaoResponse"
+]
