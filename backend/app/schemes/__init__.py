9ee352b20569635f73e91d8cad70ab776dadc498
jpaullopes <jpaullopes27@gmail.com>
2025-08-28 09:20:08 -0300

    The imports have been updated

--- backend/app/schemes/__init__.py
+++ backend/app/schemes/__init__.py
@@ -1,11 +1,11 @@
 from .comodo import ComodoCreate, ComodoUpdate, ComodoResponse
 from .dispositivo import DispositivoCreate, DispositivoCreateInRoom, DispositivoUpdate, DispositivoResponse
-from .cena import CenaCreate, CenaUpdate, CenaResponse
+from .cena import CenaCreate, CenaUpdate, CenaResponse, CenaExecucaoResponse
 from .acao import AcaoCreate, AcaoCreateInScene, AcaoUpdate, AcaoResponse
 
 __all__ = [
     "ComodoCreate", "ComodoUpdate", "ComodoResponse",
     "DispositivoCreate", "DispositivoCreateInRoom", "DispositivoUpdate", "DispositivoResponse",
-    "CenaCreate", "CenaUpdate", "CenaResponse",
+    "CenaCreate", "CenaUpdate", "CenaResponse", "CenaExecucaoResponse",
     "AcaoCreate", "AcaoCreateInScene", "AcaoUpdate", "AcaoResponse"
 ]
