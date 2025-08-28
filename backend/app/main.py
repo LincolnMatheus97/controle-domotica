c58212297ac76ea8b3b5d7d4acb5c3543700d7ff
jpaullopes <jpaullopes27@gmail.com>
2025-08-28 09:41:48 -0300

    on_load -> lifespan

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+from fastapi.lifecycle import Lifespan
 from app.api.endpoints import comodo_router, dispositivo_router, cena_router, acao_router
 from app.database.database import init_db
 import uvicorn
+
+def lifespan(app: FastAPI) -> Lifespan:
+    async def startup():
+        init_db()  
+    async def shutdown():
+        pass  
+    return Lifespan(on_startup=[startup], on_shutdown=[shutdown])
+
 
 # Criar a aplicação FastAPI
 app = FastAPI(
     title="Sistema de Controle Domótico",
     docs_url="/docs",  # Swagger 
-    redoc_url="/redoc"  
+    redoc_url="/redoc",
+    lifespan=lifespan
 )
 
 # Incluindo as rotas
@@ -15,11 +25,6 @@ app.include_router(comodo_router, prefix="/api", tags=["comodos"])
 app.include_router(dispositivo_router, prefix="/api", tags=["dispositivos"])
 app.include_router(cena_router, prefix="/api", tags=["cenas"])
 app.include_router(acao_router, prefix="/api", tags=["acoes"])
-
-# Criar as tabelas no startup
-@app.on_event("startup")
-def startup_event():
-    init_db()
 
 # Endpoint básico
 @app.get("/")
