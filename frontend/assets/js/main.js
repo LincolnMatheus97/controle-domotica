56b8dd1c4cb52b2e09353748c6240f00a06e20f1
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-08-29 15:44:52 -0300

    :truck: chore: Atualiza os event listeners da main

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,5 +1,6 @@
 import { getById } from "./utils.js";
 import { renderizarComodos, lidarAddComodo, lidarAcoesDosComodos } from "./ui/comodos.js";
+import { lidarAcoesDosDispositivos, lidarAdicionarDispositivo } from "./ui/dispositivos.js";
 
 
 function init() {
@@ -9,8 +10,34 @@ function init() {
     const comodosList = getById('comodos-list');
 
     addComodoButton.addEventListener('click', lidarAddComodo);
-    comodosList.addEventListener('click', lidarAcoesDosComodos);
+    
+    // Listener principal (orquestrador)
+    comodosList.addEventListener('click', (event) => {
+        const target = event.target;
 
+        if (target.classList.contains('btn-add-dispositivo')) {
+            lidarAdicionarDispositivo(event);
+
+        } else if (target.closest('.dispositivo-item')) {
+            lidarAcoesDosDispositivos(event);
+
+        } else if (target.closest('.comodo-item')) {
+            lidarAcoesDosComodos(event);
+        }
+    });
+
+    // Listener secundario 
+    document.addEventListener('click', (event) => {
+        const target = event.target;
+        if (!target.closest('#comodos-section') && !target.closest('#confirm-modal')) {
+            const comodosExpandidos = document.querySelectorAll('.comodo-item.expanded');
+            comodosExpandidos.forEach(comodo => {
+                comodo.classList.remove('expanded');
+            });
+        }
+    });
+
+    // Renderiza a lista inicial de cômodos
     renderizarComodos();
 }
 
