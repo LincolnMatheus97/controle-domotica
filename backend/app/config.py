7563ec1a0f4043f53c21ae1bb60562769d3ce002
jpaullopes <jpaullopes27@gmail.com>
2025-09-02 10:31:10 -0300

    Docs: Add comments to config.py for clarity. Added comments to explain the purpose of imports and the DATABASE_URL configuration.

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,12 @@
+# app/config.py
+
+# Importa o módulo os para interagir com o sistema operacional
 import os
+# Importa a função load_dotenv do pacote dotenv para carregar variáveis de ambiente
 from dotenv import load_dotenv
 
+# Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
 
+# Define a URL do banco de dados, obtendo-a das variáveis de ambiente ou usando um valor padrão
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./data/domotica.db")
